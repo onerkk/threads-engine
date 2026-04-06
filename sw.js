09869952f7734553daf433084add2f81dd275860
onerkk <onerkk@gmail.com>
2026-04-06 08:52:23 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'jy-engine-v1';
+const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).then(r => {
+    const c = r.clone();
+    caches.open(CACHE).then(cache => cache.put(e.request, c));
+    return r;
+  }).catch(() => caches.match(e.request)));
+});
